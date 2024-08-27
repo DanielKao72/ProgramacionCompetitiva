53c6da989acf27a9f8826d0c142fff343a2a4686
Juan Daniel Kao Pech <a19201340@alumnos.uady.mx>
2024-08-26 23:57:13 -0600

    chore: :hammer: add first part of OnlyPluses solution

---
+++ Solucion_Problemset/OnlyPluses.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define el "\n"
+#define ll long long
+#define yes cout << "YES" << el
+#define no cout << "NO" << el
+#define vll vector<ll>
+
+void solve();
+
+int main(){
+    ll t; cin>>t;
+    
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
+void solve(){
+    //TODO
+    return;
+}
