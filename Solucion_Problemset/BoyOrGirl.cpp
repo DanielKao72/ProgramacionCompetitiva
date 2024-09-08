fb632b8cdb59a0da2e256d744d92acd5904ef72d
Juan Daniel Kao Pech <a19201340@alumnos.uady.mx>
2024-09-08 14:29:53 -0600

    feat: :hammer: add Boy or Girl solution

---
+++ Solucion_Problemset/BoyOrGirl.cpp
@@ -0,0 +1,64 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define el "\n"
+#define ll long long
+#define yes cout << "YES" << el
+#define no cout << "NO" << el
+#define vll vector<ll>
+#define rep(i,a,b) for(ll i=a; i<b; i++)
+#define mll map<ll,ll>
+#define bob cout<<"Bob"<<el
+#define alice cout<<"Alice"<<el
+
+vector<char> segmentedSieve(long long L, long long R) {
+    // generate all primes up to sqrt(R)
+    long long lim = sqrt(R);
+    vector<char> mark(lim + 1, false);
+    vector<long long> primes;
+    for (long long i = 2; i <= lim; ++i) {
+        if (!mark[i]) {
+            primes.emplace_back(i);
+            for (long long j = i * i; j <= lim; j += i)
+                mark[j] = true;
+        }
+    }
+
+    vector<char> isPrime(R - L + 1, true);
+    for (long long i : primes)
+        for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    return isPrime;
+}
+
+void solve();
+
+int main(){
+    ll t; 
+    //cin>>t;
+    t = 1;
+    
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
+void solve(){
+    string s; cin>>s;
+    map<char,ll> m;
+
+    rep(i,0,s.size()){
+        m[s[i]]++;
+    }
+
+    if(m.size() % 2 == 0) cout << "CHAT WITH HER!" << el;
+    else cout << "IGNORE HIM!" << el;
+
+    return;
+}
